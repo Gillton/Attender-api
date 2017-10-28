a4aec19a725a42f9c8eeb79f6e4b6ba4cecb98e6
Adam Eaton <adameaton55@gmail.com>
2017-10-28 18:37:30 -0500

    Finished cruddy C and addded cruddy D

--- routes/classes.js
+++ routes/classes.js
@@ -3,7 +3,7 @@ import express from 'express';
 import { Client } from '@microsoft/microsoft-graph-client';
 
 import Class from '../models/Class';
-import Person from '../models/Person'
+import Person from '../models/Person';
 
 let router = express.Router();
 
@@ -24,7 +24,7 @@ router.get('/', (req, res) => {
                 res.sendStatus(401);
             } else {
                 Class.find({ instructor: info.id }, (err, classes) => {
-                    Class.populate(classes, {path: 'instructor'}, (err, classes) => {
+                    Class.populate(classes, { path: 'instructor' }, (err, classes) => {
                         if (err)
                             console.log(err);
                         res.json(classes);
@@ -53,19 +53,64 @@ router.put('/:id', (req, res) => {
             } else {
                 let classId = req.params.id;
                 let newClass = req.body;
-                
+
                 if (classId === 'new') {
-                    console.log('New Class');
+
+                    console.log(newClass);
+                    delete newClass._id;
+                    newClass.instructor._id = info.id;
+
+                    let c = new Class(newClass);
+
+                    c.save((err, c) => {
+                        if (err)
+                            console.log(err)
+                        Class.populate(c, { path: 'instructor' }, (err, c) => {
+                            if (err)
+                                console.log(err);
+                            res.json(c);
+                        });
+                    });
+
                 } else {
-                    Class.update({ _id: classId}, newClass, (err, c) => {
+                    Class.update({ _id: classId }, newClass, (err, c) => {
                         if (err)
                             console.log(err);
-                        res.json(c);
+                        res.json(newClass);
                     });
                 }
             }
         });
-})
+});
+
+router.delete('/:id', (req, res) => {
+    let access_token = req.header('Authorization');
+    let client = Client.init({
+        debugLogging: true,
+        authProvider: (done) => {
+            done(null, access_token);
+        }
+    });
+
+    client
+        .api('/me')
+        .select('id')
+        .get((err, info) => {
+            if (err) {
+                res.sendStatus(401);
+            } else {
+                let classId = req.params.id;
+                let newClass = req.body;
+
+                Class.find({_id: classId}).remove().exec((err, c) => {
+                    if (err)
+                        console.log(err);
+                    res.json(classId);
+                });
+
+            }
+        });
+});
 
 router.get('/:id', (req, res) => {
     res.sendStatus(200);
